1f7cd826deeb528735d3676de612cb2e829a9f26
Cahyo Ade <cahyoade8@gmail.com>
2022-08-08 22:21:19 +0700

    minor changes to cartTtem layout

--- frontend/src/components/sales/CartItem.js
+++ frontend/src/components/sales/CartItem.js
@@ -11,16 +11,16 @@ function CartItem(props) {
     })
 
     return ( 
-        <div className='grid grid-cols-4 items-center w-full py-2 border-b-2'>
-            <div className='pr-2'>
+        <div className='grid grid-cols-8 items-center w-full py-2 border-b-2'>
+            <div className='pr-2 col-span-2'>
                 <p>{props.name}</p>
                 <p className='italic text-base'>Rp{formatter(props.prices[priceIndex].price)}</p>
             </div>
-            <div className='grid grid-cols-2'>
+            <div className='flex justify-end px-4 col-span-3'>
                 <input type="number" value={props.count} onChange={e => props.changeItemCount({name : props.name}, e.target.value === '' || e.target.value < 0 ? 0 : +e.target.value)} className='h-[30px] px-[10px] w-full self-center rounded-lg bg-neutral-100 border border-solid border-neutral-300'></input>
                 <p className='italic text-base self-center ml-2'>{props.unit}</p>
             </div>
-            <p className='justify-self-end'>Rp{formatter(props.count * props.prices[priceIndex].price)}</p>
+            <p className='justify-self-end col-span-2'>Rp{formatter(props.count * props.prices[priceIndex].price)}</p>
             <img src={delIcon} alt='delIcon' onClick={() => props.removeProductFromCart(props)} className='cursor-pointer justify-self-end'></img>
         </div>
     );
